f8f4287a72e54ae4985fca482815a9c5c741818d
Nastya <killer99whale@mail.ru>
2024-02-06 19:40:20 +0300

    primer

---
+++ form.php
@@ -0,0 +1,6 @@
+<?php
+// print_r($_POST);
+// echo $_POST['name'];
+// echo $_POST['surname'];
+
+echo json_encode($_POST);
---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ajax</title>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script>
+        // Вместо document.ready
+        $(function(){
+            $("form").on("submit", function(event) {
+                event.preventDefault();
+                $.post("/form.php", $( this ).serialize(), function (data) {
+                    $('#result').html(data.name);
+
+                }, 'json');
+            })
+        });
+    </script>
+</head>
+<body>
+    <form method="POST">
+        <label for="fname">First name:</label><br>
+        <input type="text" id="name" name="name" value="John"><br>
+        <label for="lname">Last name:</label><br>
+        <input type="text" id="surname" name="surname" value="Doe"><br><br>
+        <input type="submit" value="Submit">
+    </form> 
+    <div id="result"></div>
+</body>
+</html>
